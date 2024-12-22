32eeb21e828675353cce3e176947614728991c46
Andrhan_04 <andrham2004@yandex.ru>
2024-12-22 21:43:29 +0800

    add Templates

---
+++ Templates-master/Templates.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include <cmath> 
+#include <sstream> 
+#include <fstream> 
+#include <vector> 
+#include <string> 
+#include <list> 
+#include <queue> 
+#include <set> 
+#include <map> 
+#include <algorithm> 
+#include <numeric> 
+#include <functional> 
+ 
+using namespace std;
+string yes = "YES", no = "NO";
+#define int long long
+#define Fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define all(v) (v).begin() , (v).end()
+#define para pair<long long,long long>
+int INF = 1e9 + 7;
+#define Graf vector<vector<int>>
+#pragma comment(linker, "/STACK:256000000")
+
+
+signed main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
